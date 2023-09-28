855d5a2f6b05c62aec22168e7ded053492c0f59d
ADMIN <ADMIN@LAPTOP-ONSCREEN>
2023-09-28 21:16:44 +0700

    Add project files.

---
+++ cacular/caculator.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace cacular
+{
+    partial class caculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TextBox = new TextBox();
+            btnb3 = new Button();
+            btequal = new Button();
+            btdell = new Button();
+            btnb6 = new Button();
+            btnb5 = new Button();
+            btnb4 = new Button();
+            btnb1 = new Button();
+            btnb2 = new Button();
+            btnb9 = new Button();
+            btnb8 = new Button();
+            btnb7 = new Button();
+            btsubtraction = new Button();
+            btaddition = new Button();
+            btdivision = new Button();
+            btmultiplication = new Button();
+            bt = new Button();
+            btnb0 = new Button();
+            bt20 = new Button();
+            SuspendLayout();
+            // 
+            // TextBox
+            // 
+            TextBox.Location = new Point(11, 12);
+            TextBox.MaximumSize = new Size(500, 500);
+            TextBox.MinimumSize = new Size(0, 50);
+            TextBox.Name = "TextBox";
+            TextBox.Size = new Size(240, 50);
+            TextBox.TabIndex = 1;
+            TextBox.Text = "0";
+            TextBox.TextChanged += tb1_TextChanged;
+            // 
+            // btnb3
+            // 
+            btnb3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb3.Location = new Point(124, 118);
+            btnb3.Name = "btnb3";
+            btnb3.Size = new Size(50, 44);
+            btnb3.TabIndex = 2;
+            btnb3.Text = "3";
+            btnb3.UseVisualStyleBackColor = true;
+            btnb3.Click += btnb3_Click;
+            // 
+            // btequal
+            // 
+            btequal.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btequal.Location = new Point(12, 68);
+            btequal.Name = "btequal";
+            btequal.Size = new Size(162, 44);
+            btequal.TabIndex = 3;
+            btequal.Text = "=";
+            btequal.UseVisualStyleBackColor = true;
+            btequal.Click += btequal_Click;
+            // 
+            // btdell
+            // 
+            btdell.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btdell.Location = new Point(201, 68);
+            btdell.Name = "btdell";
+            btdell.Size = new Size(50, 44);
+            btdell.TabIndex = 4;
+            btdell.Text = "dell";
+            btdell.UseVisualStyleBackColor = true;
+            btdell.Click += btdell_Click;
+            // 
+            // btnb6
+            // 
+            btnb6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb6.Location = new Point(124, 168);
+            btnb6.Name = "btnb6";
+            btnb6.Size = new Size(50, 44);
+            btnb6.TabIndex = 6;
+            btnb6.Text = "6";
+            btnb6.UseVisualStyleBackColor = true;
+            btnb6.Click += btnb6_Click;
+            // 
+            // btnb5
+            // 
+            btnb5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb5.Location = new Point(68, 168);
+            btnb5.Name = "btnb5";
+            btnb5.Size = new Size(50, 44);
+            btnb5.TabIndex = 7;
+            btnb5.Text = "5";
+            btnb5.UseVisualStyleBackColor = true;
+            btnb5.Click += btnb5_Click;
+            // 
+            // btnb4
+            // 
+            btnb4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb4.Location = new Point(12, 168);
+            btnb4.Name = "btnb4";
+            btnb4.Size = new Size(50, 44);
+            btnb4.TabIndex = 8;
+            btnb4.Text = "4";
+            btnb4.UseVisualStyleBackColor = true;
+            btnb4.Click += btnb4_Click;
+            // 
+            // btnb1
+            // 
+            btnb1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb1.Location = new Point(11, 118);
+            btnb1.Name = "btnb1";
+            btnb1.Size = new Size(50, 44);
+            btnb1.TabIndex = 9;
+            btnb1.Text = "1";
+            btnb1.UseVisualStyleBackColor = true;
+            btnb1.Click += btnb1_Click;
+            // 
+            // btnb2
+            // 
+            btnb2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb2.Location = new Point(68, 118);
+            btnb2.Name = "btnb2";
+            btnb2.Size = new Size(50, 44);
+            btnb2.TabIndex = 10;
+            btnb2.Text = "2";
+            btnb2.UseVisualStyleBackColor = true;
+            btnb2.Click += btnb2_Click;
+            // 
+            // btnb9
+            // 
+            btnb9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb9.Location = new Point(124, 218);
+            btnb9.Name = "btnb9";
+            btnb9.Size = new Size(50, 44);
+            btnb9.TabIndex = 11;
+            btnb9.Text = "9";
+            btnb9.UseVisualStyleBackColor = true;
+            btnb9.Click += btnb9_Click;
+            // 
+            // btnb8
+            // 
+            btnb8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb8.Location = new Point(67, 218);
+            btnb8.Name = "btnb8";
+            btnb8.Size = new Size(50, 44);
+            btnb8.TabIndex = 12;
+            btnb8.Text = "8";
+            btnb8.UseVisualStyleBackColor = true;
+            btnb8.Click += btnb8_Click;
+            // 
+            // btnb7
+            // 
+            btnb7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb7.Location = new Point(11, 218);
+            btnb7.Name = "btnb7";
+            btnb7.Size = new Size(50, 44);
+            btnb7.TabIndex = 13;
+            btnb7.Text = "7";
+            btnb7.UseVisualStyleBackColor = true;
+            btnb7.Click += btnb7_Click;
+            // 
+            // btsubtraction
+            // 
+            btsubtraction.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btsubtraction.Location = new Point(201, 168);
+            btsubtraction.Name = "btsubtraction";
+            btsubtraction.Size = new Size(50, 44);
+            btsubtraction.TabIndex = 14;
+            btsubtraction.Text = "-";
+            btsubtraction.UseVisualStyleBackColor = true;
+            btsubtraction.Click += btsubtraction_Click;
+            // 
+            // btaddition
+            // 
+            btaddition.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btaddition.Location = new Point(201, 118);
+            btaddition.Name = "btaddition";
+            btaddition.Size = new Size(50, 44);
+            btaddition.TabIndex = 15;
+            btaddition.Text = "+";
+            btaddition.UseVisualStyleBackColor = true;
+            btaddition.Click += btaddition_Click;
+            // 
+            // btdivision
+            // 
+            btdivision.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btdivision.Location = new Point(201, 268);
+            btdivision.Name = "btdivision";
+            btdivision.Size = new Size(50, 44);
+            btdivision.TabIndex = 16;
+            btdivision.Text = "/";
+            btdivision.UseVisualStyleBackColor = true;
+            btdivision.Click += btdivision_Click;
+            // 
+            // btmultiplication
+            // 
+            btmultiplication.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btmultiplication.Location = new Point(201, 218);
+            btmultiplication.Name = "btmultiplication";
+            btmultiplication.Size = new Size(50, 44);
+            btmultiplication.TabIndex = 17;
+            btmultiplication.Text = "x";
+            btmultiplication.UseVisualStyleBackColor = true;
+            btmultiplication.Click += btmultiplication_Click;
+            // 
+            // bt
+            // 
+            bt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bt.Location = new Point(11, 268);
+            bt.Name = "bt";
+            bt.Size = new Size(50, 44);
+            bt.TabIndex = 18;
+            bt.Text = ".";
+            bt.UseVisualStyleBackColor = true;
+            bt.Click += btdot_Click;
+            // 
+            // btnb0
+            // 
+            btnb0.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnb0.Location = new Point(67, 268);
+            btnb0.Name = "btnb0";
+            btnb0.Size = new Size(50, 44);
+            btnb0.TabIndex = 19;
+            btnb0.Text = "0";
+            btnb0.UseVisualStyleBackColor = true;
+            btnb0.Click += btnb0_Click;
+            // 
+            // bt20
+            // 
+            bt20.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bt20.Location = new Point(124, 268);
+            bt20.Name = "bt20";
+            bt20.Size = new Size(50, 44);
+            bt20.TabIndex = 20;
+            bt20.Text = "+/-";
+            bt20.UseVisualStyleBackColor = true;
+            bt20.Click += btminusplus_Click;
+            // 
+            // caculator
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(263, 365);
+            Controls.Add(bt20);
+            Controls.Add(btnb0);
+            Controls.Add(bt);
+            Controls.Add(btmultiplication);
+            Controls.Add(btdivision);
+            Controls.Add(btaddition);
+            Controls.Add(btsubtraction);
+            Controls.Add(btnb7);
+            Controls.Add(btnb8);
+            Controls.Add(btnb9);
+            Controls.Add(btnb2);
+            Controls.Add(btnb1);
+            Controls.Add(btnb4);
+            Controls.Add(btnb5);
+            Controls.Add(btnb6);
+            Controls.Add(btdell);
+            Controls.Add(btequal);
+            Controls.Add(btnb3);
+            Controls.Add(TextBox);
+            Name = "caculator";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox TextBox;
+        private Button btnb3;
+        private Button btequal;
+        private Button btdell;
+        private Button btnb6;
+        private Button btnb5;
+        private Button btnb4;
+        private Button btnb1;
+        private Button btnb2;
+        private Button btnb9;
+        private Button btnb8;
+        private Button btnb7;
+        private Button btsubtraction;
+        private Button btaddition;
+        private Button btdivision;
+        private Button btmultiplication;
+        private Button bt;
+        private Button btnb0;
+        private Button bt20;
+    }
+}
---
+++ cacular/caculator.cs
@@ -0,0 +1,225 @@
+﻿
+
+
+
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics.Eventing.Reader;
+using System.Text.RegularExpressions;
+
+namespace cacular
+{
+    public partial class caculator : Form
+    {
+        private decimal valueFirst = 0.0m;
+        private decimal valueSecond = 0.0m;
+        private decimal result = 0.0m;
+        private string operators = "+";
+        public caculator()
+        {
+            InitializeComponent();
+        }
+
+        private void tb1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btdot_Click(object sender, EventArgs e)
+        {
+            if (!TextBox.Text.Contains("."))
+            {
+                TextBox.Text += ".";
+            }
+        }
+        private void btnb0_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "0";
+            }
+            else
+            {
+                TextBox.Text += "0";
+            }
+        }
+        private void btnb1_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "1";
+            }
+            else
+            {
+                TextBox.Text += "1";
+            }
+        }
+        private void btnb2_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "2";
+            }
+            else
+            {
+                TextBox.Text += "2";
+            }
+        }
+        private void btnb3_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "3";
+            }
+            else
+            {
+                TextBox.Text += "3";
+            }
+        }
+        private void btnb4_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "4";
+            }
+            else
+            {
+                TextBox.Text += "4";
+            }
+        }
+        private void btnb5_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "5";
+            }
+            else
+            {
+                TextBox.Text += "5";
+            }
+        }
+        private void btnb6_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "6";
+            }
+            else
+            {
+                TextBox.Text += "6";
+            }
+        }
+        private void btnb7_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "7";
+            }
+            else
+            {
+                TextBox.Text += "7";
+            }
+        }
+        private void btnb8_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "8";
+            }
+            else
+            {
+                TextBox.Text += "8";
+            }
+        }
+        private void btnb9_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+            {
+                TextBox.Text = "9";
+            }
+            else
+            {
+                TextBox.Text += "9";
+            }
+        }
+        private void btminusplus_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text.Contains("-"))
+            {
+                TextBox.Text = TextBox.Text.Trim('-');
+            }
+            else
+            {
+                TextBox.Text = '-' + TextBox.Text;
+            }
+        }
+        private void btaddition_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TextBox.Text);
+            TextBox.Clear();
+            operators = "+";
+        }
+        private void btsubtraction_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TextBox.Text);
+            TextBox.Clear();
+            operators = "-";
+        }
+        private void btmultiplication_Click(object sender, EventArgs e)
+        {
+            valueFirst = decimal.Parse(TextBox.Text);
+            TextBox.Clear();
+            operators = "x";
+        }
+
+        private void btdivision_Click(object sender, EventArgs e)
+        {
+
+            valueFirst = decimal.Parse(TextBox.Text);
+            TextBox.Clear();
+            operators = "/";
+        }
+        private void btequal_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBox.Text))
+            {
+                valueSecond = decimal.Parse(TextBox.Text);
+
+                // Thực hiện phép tính dựa vào operators
+                switch (operators)
+                {
+                    case "+":
+                        result = valueFirst + valueSecond;
+                        break;
+                    case "-":
+                        result = valueFirst - valueSecond;
+                        break;
+                    case "x":
+                        result = valueFirst * valueSecond;
+                        break;
+                    case "/":
+                        if (valueSecond != 0)
+                        {
+                            result = valueFirst / valueSecond;
+                        }
+                        else
+                        {
+                            // xu ly loi chia cho 0
+                            TextBox.Text = "Lỗi: Không thể chia cho 0";
+                            return;
+                        }
+                        break;
+                }
+
+                TextBox.Text = result.ToString();
+            }
+
+        }
+
+        private void btdell_Click(object sender, EventArgs e)
+        {
+            valueFirst = 0.0m;
+            valueSecond = 0.0m;
+            TextBox.Text = "0";
+        }
+    }
+}
